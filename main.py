106ddbd6719e4c7037fd31098efa1be0dedce9f9
KhushiMK <100280301+KhushiMK@users.noreply.github.com>
2025-07-25 07:06:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,5 +102,5 @@ def predictor():
 if __name__ == '__main__':
     app.run(debug=True)
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="127.0.0.1", port=port)
 
